8422249f17f0286ca9081c8cae1f00c0837296d1
pushLiLi <166570816+pushLiLi@users.noreply.github.com>
2024-10-07 22:29:24 +0800

    第二章讨论第二题

--- abc.py
+++ abc.py
@@ -3,21 +3,37 @@ class ListNode:
         self.val = val
         self.next = next
 
-def reverse_list(head):
-    new = None
-    a = head
-    while a:
-        b = a.next
-        a.next = new
-        new = a
-        a = b
-    return new
+
+def InsertNodeBeforeValue(head,node):
+    #链表头部
+    if head.val == node.val:
+        node.next = head
+        return node
+    #链表中间
+    cur = head
+    while cur.next != None and cur.next.val != node.val:
+        cur = cur.next
+    #错误处理
+    if cur.next == None:
+        print("找不到指定元素")
+    node.next = cur.next
+    cur.next = node
+
+    return head
+
+
+
+    return head
+
+
+
 
 def print_list(head):
     while head:
         print(head.val, end=" ")
         head = head.next
     print()
+
 
 # 创建链表 1->2->3->4->5
 head = ListNode(1)
@@ -29,8 +45,9 @@ head.next.next.next.next = ListNode(5)
 print("原始链表:")
 print_list(head)
 
-# 翻转链表
-head = reverse_list(head)
+# 在值为3的节点前插入值为6的节点
+node_to_insert = ListNode(3)
+head = InsertNodeBeforeValue(head, node_to_insert)
 
-print("翻转后的链表:")
+print("插入后的链表:")
 print_list(head)
